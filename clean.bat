d96e020a7f4c743b17d2d15023d52be860ab5de8
Kromster80 <kromster80@gmail.com>
2011-01-24 14:53:09 +0000

    Minor updates in Docs
    git-svn-id: svn://localhost/trunk@1369 a386ff7f-d321-c144-bb17-e251d28fee14

--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
 erase /F /Q /S *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S *.o *.or *.ppu *.compiled
-erase /F /Q /S *.log *.tmp *.db *.skincfg
+erase /F /Q /S *.log *.tmp *.db *.ion *.skincfg
 erase /F /Q /S /A:H *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S /A:H *.o *.or *.ppu *.compiled
-erase /F /Q /S /A:H *.log *.tmp *.db *.skincfg
+erase /F /Q /S /A:H *.log *.tmp *.db *.ion *.skincfg
