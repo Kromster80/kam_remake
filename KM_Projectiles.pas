a362d44f4a453c3fe044b370adbb82debc7c99d1
Kromster80 <kromster80@gmail.com>
2011-06-28 19:08:46 +0000

    More work WIP
    git-svn-id: svn://localhost/trunk@1856 a386ff7f-d321-c144-bb17-e251d28fee14

--- KM_Projectiles.pas
+++ KM_Projectiles.pas
@@ -71,66 +71,57 @@ end;
 
 
 function TKMProjectiles.AimTarget(aStart:TKMPointF; aTarget:TKMUnit; aProjType:TProjectileType; aOwner:TPlayerIndex):word;
-var i:integer; ArrowVector,TargetVector,Target:TKMPointF; ApproachSpeed,TimeToHit:single;
+var i:integer; ArrowVector,TargetVector,Target,TargetPosition:TKMPointF; TimeToHit:single;
+ A,B,C,D:single;
+ Time1, Time2: single;
 begin
   i := AddNewProjectile(aProjType);
 
   //Now we know projectiles speed and aim, we can predict where target will be at the time projectile hits it
 
-  //I wonder if medieval archers knew about vectors
-
+  //I wonder if medieval archers knew about vectors and quadratic equations
 
   TargetVector := aTarget.GetMovementVector;
 
-  {
-    Target.X := aTarget.PositionF.X + TargetVector.X * Time;
-    Target.Y := aTarget.PositionF.Y + TargetVector.Y * Time;
+  { Target.X := Position.X + Vector.X * Time;
+    Target.Y := Position.Y + Vector.Y * Time;
 
     FlightDistance := ArrowSpeed * Time;
 
-    DistanceToArrow := GetLength(aStart, Target) - FlightDistance;
-
     sqr(Target.X)+sqr(Target.Y) = sqr(FlightDistance);
 
-    sqr(aTarget.PositionF.X + TargetVector.X * Time) + sqr(aTarget.PositionF.Y + TargetVector.Y * Time) = sqr(ArrowSpeed * Time)
+    sqr(Position.X + Vector.X * Time) + sqr(Position.Y + Vector.Y * Time) = sqr(ArrowSpeed * Time)
 
-    sqr(aTarget.PositionF.X) + 2 * Time * aTarget.PositionF.X * TargetVector.X + sqr(Time) * sqr(TargetVector.X) +
-    sqr(aTarget.PositionF.Y) + 2 * Time * aTarget.PositionF.Y * TargetVector.Y + sqr(Time) * sqr(TargetVector.Y) =
+    sqr(Position.X) + 2 * Time * Position.X * Vector.X + sqr(Time) * sqr(Vector.X) +
+    sqr(Position.Y) + 2 * Time * Position.Y * Vector.Y + sqr(Time) * sqr(Vector.Y) =
     sqr(ArrowSpeed) * sqr(Time)
 
-    sqr(aTarget.PositionF.X) + sqr(aTarget.PositionF.Y) +
-    2 * Time * (aTarget.PositionF.X * TargetVector.X + aTarget.PositionF.Y * TargetVector.Y) +
-    sqr(Time) * (sqr(TargetVector.X) + sqr(TargetVector.Y) - sqr(ArrowSpeed)) = 0
+    sqr(Time) * (sqr(Vector.X) + sqr(Vector.Y) - sqr(ArrowSpeed)) +
+    2 * Time * (Position.X * Vector.X + Position.Y * Vector.Y) +
+    sqr(Position.X) + sqr(Position.Y) = 0
+
+    //Lets try to solve this quadratic equation
+    //ATT + BT + C = 0
+    //by using formulae X = (-B +- sqrt(B*B - 4*A*C)) / 2*A
+    A = sqr(Vector.X) + sqr(Vector.Y) - sqr(ArrowSpeed)
+    B = 2 * (Position.X * Vector.X + Position.Y * Vector.Y)
+    C = sqr(Position.X) + sqr(Position.Y) }
+
+  TargetPosition.X := (aTarget.PositionF.X - aStart.X);
+  TargetPosition.Y := (aTarget.PositionF.Y - aStart.Y);
+
+  A := sqr(TargetVector.X) + sqr(TargetVector.Y) - sqr(fItems[i].fSpeed);
+  B := 2 * (TargetPosition.X * TargetVector.X + TargetPosition.Y * TargetVector.Y);
+  C := sqr(TargetPosition.X) + sqr(TargetPosition.Y);
+
+  D := sqr(B) - 4 * A * C;
+
+  Time1 := (-B + sqrt(D)) / (2 * A);
+  Time2 := (-B - sqrt(D)) / (2 * A);
 
 
-
-
-
-    D + A * Time + B * sqr(Time) = C * sqr(Time)
-
-    D + A * Time - B * sqr(Time) = 0
-
-    a-b * a-b = a*a-2ab+b
-
-    sqr(A - Time) = 0
-
-  }
-
-  //Get arrows and targets vector per 1 tick (vector length = speed)
-  ArrowVector.X := (aTarget.PositionF.X - aStart.X);
-  ArrowVector.Y := (aTarget.PositionF.Y - aStart.Y);
-  ArrowVector := Normalize(ArrowVector);
-  ArrowVector.X := ArrowVector.X * fItems[i].fSpeed;
-  ArrowVector.Y := ArrowVector.Y * fItems[i].fSpeed;
-
-  //Now we need to know speed of approach
-  ApproachSpeed := GetLength(ArrowVector.X-TargetVector.X, ArrowVector.Y-TargetVector.Y);
-
-  //Compute estimated time to hit
-  TimeToHit := GetLength(aStart, aTarget.PositionF) / ApproachSpeed;
-
-  Target.X := aTarget.PositionF.X + TargetVector.X * TimeToHit;
-  Target.Y := aTarget.PositionF.Y + TargetVector.X * TimeToHit;
+  Target.X := aTarget.PositionF.X{ + Time1*Time2/100000};
+  Target.Y := aTarget.PositionF.Y;
 
   Result := AddItem(i, aStart, Target, aProjType, aOwner);
 end;
