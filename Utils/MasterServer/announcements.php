8c81b1c8397d002dfee930b5189f6902f1ac5919
LewinJH <lewinjh@gmail.com>
2011-10-04 14:11:59 +0000

    The Knight updated the German translation :)
    git-svn-id: svn://localhost/trunk@2348 a386ff7f-d321-c144-bb17-e251d28fee14

--- Utils/MasterServer/announcements.php
+++ Utils/MasterServer/announcements.php
@@ -13,8 +13,11 @@ if($Rev != $GAME_VERSION)
 		/*case 'ger':
 			echo "";
 			break;*/
+		case 'ger':
+			echo "Deine Version des Remakes ist veraltet! Du hast ".$Rev.", die neuste ist ".$GAME_VERSION.".|Solange du nicht die aktuelle Version hast, kannst du nicht online spielen.||Bitte lade das neuste Update von www.kamremake.com runter.";
+			break;
 		default:
-			echo "Your KaM Remake version is out of date! You are running ".$Rev." but the most recent version is ".$GAME_VERSION.".|You cannot play online until you have updated.||Please download the update at: http://castlesand.googlecode.com/";
+			echo "Your KaM Remake version is out of date! You are running ".$Rev." but the most recent version is ".$GAME_VERSION.".|You cannot play online until you have updated.||Please download the update at: www.kamremake.com";
 	}
 }
 else
@@ -25,7 +28,7 @@ else
 			echo "";
 			break;*/
 		default:
-			echo "Welcome to the Knights and Merchants Remake online!||Weekly matches are currently run every Sunday at 9pm Central European Time. Please join us if you can!|Thank you for your support!";
+			echo "Welcome to the Knights and Merchants Remake online!||Weekly matches are currently run every Saturday at 9pm Central European Time. Please join us if you can!|Thank you for your support!";
 	}
 }
 ?>
