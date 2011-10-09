653be1bb8573c68a7e1247073a4d1902607e910f
LewinJH <lewinjh@gmail.com>
2011-10-09 13:30:42 +0000

    - Marketplace trade cost calculations  - Update to map from GreatWhiteBear
    git-svn-id: svn://localhost/trunk@2373 a386ff7f-d321-c144-bb17-e251d28fee14

--- KM_ResourceResource.pas
+++ KM_ResourceResource.pas
@@ -9,20 +9,22 @@ type
   TKMResourceDat = class
   private
     fType:TResourceType;
+    fMarketPrice:single;
     function GetGUIIcon: Word;
-    function GetMarketPrice: Word;
     function GetName: String;
   public
     constructor Create(aType:TResourceType);
     function IsValid:boolean;
     property GUIIcon: Word read GetGUIIcon;
-    property MarketPrice: Word read GetMarketPrice;
+    property MarketPrice: single read fMarketPrice;
+    property SetMarketPrice: single write fMarketPrice;
     property Name: String read GetName;
   end;
 
   TKMResourceCollection = class
   private
     fList: array [TResourceType] of TKMResourceDat;
+    procedure CalculateCostsTable;
     function GetResource(aIndex: TResourceType): TKMResourceDat;
   public
     constructor Create;
@@ -32,6 +34,9 @@ type
 
 
 const
+  MarketTradeOffFactor = 4; //X resources buys 1 resource of equal value
+
+
   ResourceKaMOrder: array[TResourceType] of byte = (0, //rt_None
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -58,6 +63,7 @@ begin
   inherited;
   for i:=Low(TResourceType) to High(TResourceType) do
     fList[i] := TKMResourceDat.Create(i);
+  CalculateCostsTable; //Calcuate the trade costs once
 end;
 
 
@@ -67,6 +73,42 @@ begin
   for i:=Low(TResourceType) to High(TResourceType) do
     fList[i].Free;
   inherited;
+end;
+
+
+procedure TKMResourceCollection.CalculateCostsTable;
+const
+  NonRenewableFactor = 2;
+  ProcessingCost = 1;
+begin
+  Resources[rt_Trunk].SetMarketPrice := 1;
+  Resources[rt_Stone].SetMarketPrice := (1/3)*NonRenewableFactor;
+  Resources[rt_Wood].SetMarketPrice := ProcessingCost + 0.5*Resources[rt_Trunk].MarketPrice;
+  Resources[rt_IronOre].SetMarketPrice := 1*NonRenewableFactor;
+  Resources[rt_GoldOre].SetMarketPrice := 1*NonRenewableFactor;
+  Resources[rt_Coal].SetMarketPrice := 1*NonRenewableFactor;
+  Resources[rt_Steel].SetMarketPrice := ProcessingCost + Resources[rt_IronOre].MarketPrice + Resources[rt_Coal].MarketPrice;
+  Resources[rt_Gold].SetMarketPrice := 0.5*(ProcessingCost + Resources[rt_GoldOre].MarketPrice + Resources[rt_Coal].MarketPrice);
+  Resources[rt_Wine].SetMarketPrice := 1;
+  Resources[rt_Corn].SetMarketPrice := 1;
+  Resources[rt_Flour].SetMarketPrice := ProcessingCost + Resources[rt_Corn].MarketPrice;
+  Resources[rt_Bread].SetMarketPrice := ProcessingCost + 0.5*Resources[rt_Flour].MarketPrice;
+  Resources[rt_Pig].SetMarketPrice := 0.5*(ProcessingCost + 5*Resources[rt_Corn].MarketPrice);
+  Resources[rt_Skin].SetMarketPrice := 0.5*(ProcessingCost + 5*Resources[rt_Corn].MarketPrice);
+  Resources[rt_Leather].SetMarketPrice := ProcessingCost + 0.5*Resources[rt_Skin].MarketPrice;
+  Resources[rt_Sausages].SetMarketPrice := (1/3)*(ProcessingCost + Resources[rt_Pig].MarketPrice);
+  Resources[rt_Shield].SetMarketPrice := ProcessingCost + Resources[rt_Wood].MarketPrice;
+  Resources[rt_MetalShield].SetMarketPrice := ProcessingCost + Resources[rt_Steel].MarketPrice + Resources[rt_Coal].MarketPrice;
+  Resources[rt_Armor].SetMarketPrice := ProcessingCost + Resources[rt_Leather].MarketPrice;
+  Resources[rt_MetalArmor].SetMarketPrice := ProcessingCost + Resources[rt_Steel].MarketPrice + Resources[rt_Coal].MarketPrice;
+  Resources[rt_Axe].SetMarketPrice := ProcessingCost + 2*Resources[rt_Wood].MarketPrice;
+  Resources[rt_Sword].SetMarketPrice := ProcessingCost + Resources[rt_Steel].MarketPrice + Resources[rt_Coal].MarketPrice;
+  Resources[rt_Pike].SetMarketPrice := ProcessingCost + 2*Resources[rt_Wood].MarketPrice;
+  Resources[rt_Hallebard].SetMarketPrice := ProcessingCost + Resources[rt_Steel].MarketPrice + Resources[rt_Coal].MarketPrice;
+  Resources[rt_Bow].SetMarketPrice := ProcessingCost + 2*Resources[rt_Wood].MarketPrice;
+  Resources[rt_Arbalet].SetMarketPrice := ProcessingCost + Resources[rt_Steel].MarketPrice + Resources[rt_Coal].MarketPrice;
+  Resources[rt_Horse].SetMarketPrice := ProcessingCost + 5*Resources[rt_Corn].MarketPrice;
+  Resources[rt_Fish].SetMarketPrice := 0.5*NonRenewableFactor;
 end;
 
 
@@ -90,12 +132,6 @@ begin
     Result := 351 + ResourceKaMOrder[fType]
   else
     Result := 41; //Show "Question mark"
-end;
-
-
-function TKMResourceDat.GetMarketPrice: Word;
-begin
-  Result := 1;
 end;
 
 
