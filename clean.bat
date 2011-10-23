9cf0dd19d67ce997b89d9c3fcdac75c55d24f4fc
Kromster80 <kromster80@gmail.com>
2011-10-23 13:12:04 +0000

    Updated cleanup.bat to preserve SVN1.7+ db files
    git-svn-id: svn://localhost/trunk@2449 a386ff7f-d321-c144-bb17-e251d28fee14

--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
 erase /F /Q /S *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S *.o *.or *.ppu *.compiled
-erase /F /Q /S *.tmp *.log *.db *.ion *.skincfg
+erase /F /Q /S *.tmp *.log thumbs.db *.ion *.skincfg
 erase /F /Q /S /A:H *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S /A:H *.o *.or *.ppu *.compiled
-erase /F /Q /S /A:H *.tmp *.log *.db *.ion *.skincfg
+erase /F /Q /S /A:H *.tmp *.log thumbs.db *.ion *.skincfg
