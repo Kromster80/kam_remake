1b31304786ffa93b664c056dd6067699a7c632c6
LewinJH <lewinjh@gmail.com>
2011-12-01 13:38:36 +0000

    - Minor update to the Italian translation  - trb1914 wrote a doc with a suggestion for AI scripting, might be useful when we get that far
    git-svn-id: svn://localhost/trunk@2634 a386ff7f-d321-c144-bb17-e251d28fee14

--- KM_Defaults.pas
+++ KM_Defaults.pas
@@ -27,7 +27,7 @@ const
   MENU_DESIGN_Y         = 768;          //Thats the size menu was designed for. All elements are placed in this size
   MENU_SP_MAPS_COUNT    = 14;           //Number of single player maps to display in menu
 
-  GAME_REVISION         = 'r2623';       //Should be updated for every release (each time save format is changed)
+  GAME_REVISION         = 'r2634';       //Should be updated for every release (each time save format is changed)
   GAME_VERSION          = '3rd Multiplayer Demo ' + GAME_REVISION;       //Game version string displayed in menu corner
   NET_PROTOCOL_REVISON  = 'r2623';     //Clients of this version may connect to the dedicated server
 
