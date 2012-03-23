a6e2044d59f1714861a9598a311ad5a0951a8407
Kromster80 <kromster80@gmail.com>
2012-03-23 14:18:31 +0000

    WIP on Mayor
    git-svn-id: svn://localhost/trunk@3069 a386ff7f-d321-c144-bb17-e251d28fee14

--- KM_Mayor.pas
+++ KM_Mayor.pas
@@ -13,8 +13,14 @@ type
     fCityPlanner: TKMCityPlanner;
     fPathFindingRoad: TPathFindingRoad;
 
-    fSerfFactor: Single; //Serfs per house
+    fExpansive: Single;
+    fHouseDemand: array [THouseType] of Single;
+    fHouseImportance: array [THouseType] of Single;
+
     procedure CheckHouseCount;
+    procedure BuildHouse(aHouse: THouseType; aLoc: TKMPoint);
+
+    procedure RecalcImportance;
   public
     constructor Create(aPlayer: TPlayerIndex);
     destructor Destroy; override;
@@ -45,16 +51,79 @@ begin
 end;
 
 
+procedure TKMayor.RecalcImportance;
+var
+  H: THouseType;
+  P: TKMPlayer;
+begin
+  P := fPlayers[fOwner];
+
+  fHouseDemand[ht_Inn] := 1 + P.Stats.GetCitizensTrained div 50;
+  fHouseDemand[ht_School] := 1;
+
+  fHouseDemand[ht_Quary] := 2 + fExpansive;
+  fHouseDemand[ht_Sawmill] := 1.5 + fExpansive;
+  fHouseDemand[ht_Woodcutters] := (P.Stats.GetHouseQty(ht_Sawmill) + P.Stats.GetHouseWip(ht_Sawmill)) * 2;
+
+  fHouseDemand[ht_Bakery] := 1 + (P.Stats.GetCitizensTrained + P.Stats.GetArmyCount) / 40;
+  fHouseDemand[ht_Mill] := 1 + (P.Stats.GetHouseQty(ht_Bakery) + P.Stats.GetHouseWip(ht_Bakery)) * 1.5;
+  fHouseDemand[ht_Farm] := 1 + (P.Stats.GetHouseQty(ht_Mill) + P.Stats.GetHouseWip(ht_Mill)) * 1.5;
+
+  for H := Low(THouseType) to High(THouseType) do
+    fHouseDemand[H] := fHouseDemand[H] - P.Stats.GetHouseQty(H) - P.Stats.GetHouseWip(H);
+
+
+  //Calculate base importance 0 - means we don't need it. 1 means we need it asap
+  fHouseImportance[ht_Inn] := 1;
+  fHouseImportance[ht_School] := 1;
+
+  fHouseImportance[ht_Quary] := 1;
+  fHouseImportance[ht_Woodcutters] := 1;
+  fHouseImportance[ht_Sawmill] := 1;
+
+  fHouseImportance[ht_Bakery] := 1;
+  fHouseImportance[ht_Mill] := 1;
+  fHouseImportance[ht_Farm] := 1;
+end;
+
+
+procedure TKMayor.BuildHouse(aHouse: THouseType; aLoc: TKMPoint);
+var
+  I: Integer;
+  Store: TKMHouse;
+  P: TKMPlayer;
+  NodeList: TKMPointList;
+  RoadExists: Boolean;
+begin
+  P := fPlayers[fOwner];
+
+  P.AddHousePlan(aHouse, aLoc);
+  aLoc := KMPointBelow(aLoc);
+
+  Store := P.Houses.FindHouse(ht_Any, aLoc.X, aLoc.Y, 1, True);
+
+  NodeList := TKMPointList.Create;
+  try
+    RoadExists := fPathFindingRoad.Route_Make(aLoc, KMPointBelow(Store.GetEntrance), CanWalk, 0, nil, NodeList, False);
+
+    if RoadExists then
+      for I := 0 to NodeList.Count - 1 do
+        //We must check if we can add the plan ontop of plans placed earlier in this turn
+        if P.CanAddFieldPlan(NodeList[I], ft_Road) then
+          P.BuildList.FieldworksList.AddField(NodeList[I], ft_Road);
+  finally
+    NodeList.Free;
+  end;
+end;
+
+
 procedure TKMayor.CheckHouseCount;
 const MandatoryHouses: array [0..2] of THouseType = (ht_Store, ht_School, ht_Inn);
 var
-  I, K: Integer;
+  I: Integer;
   H: THouseType;
-  S: TKMHouse;
   P: TKMPlayer;
   Loc: TKMPoint;
-  NodeList: TKMPointList;
-  RoadExists: Boolean;
 begin
   P := fPlayers[fOwner];
 
@@ -64,26 +133,7 @@ begin
     H := MandatoryHouses[I];
     if P.Stats.GetHouseQty(H) + P.Stats.GetHouseWip(H) = 0 then
       if fCityPlanner.FindPlaceForHouse(H, Loc) then
-      begin
-        P.AddHousePlan(H, Loc);
-
-        //GetEntrance
-        //Loc.X := Loc.X + fResource.HouseDat[H].EntranceOffsetX;
-
-        Loc := KMPointBelow(Loc);
-        S := P.Houses.FindHouse(ht_Any, Loc.X, Loc.Y, 1, True);
-
-        NodeList := TKMPointList.Create;
-        try
-          RoadExists := fPathFindingRoad.Route_Make(Loc, KMPointBelow(S.GetEntrance), CanWalk, 0, nil, NodeList, False);
-
-          if RoadExists then
-            for K := 0 to NodeList.Count - 1 do
-              P.ToggleFieldPlan(NodeList[K], ft_Road, True);
-        finally
-          FreeAndNil(NodeList);
-        end;
-      end;
+        BuildHouse(H, Loc);
   end;
 end;
 
