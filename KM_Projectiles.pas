2d296cb095814b93126e6cbd9309d16902fc6b8b
LewinJH <lewinjh@gmail.com>
2012-06-04 10:25:07 +0000

    Towers are 33% more likely to hit a moving target
    git-svn-id: svn://localhost/trunk@3454 a386ff7f-d321-c144-bb17-e251d28fee14

--- KM_Projectiles.pas
+++ KM_Projectiles.pas
@@ -53,7 +53,7 @@ const
   ProjectileArcs:array[TProjectileType,1..2] of single = ((1.6, 0.5), (1.4, 0.4), (2.5, 1), (1.2, 0.2)); //Arc curve and random fraction
   ProjectileJitter:array[TProjectileType] of single = (0.3, 0.3, 0.3, 0.2); //Fixed Jitter added every time
   //Jitter added according to target's speed (moving target harder to hit) Note: Walking = 0.1, so the added jitter is 0.1*X
-  ProjectilePredictJitter:array[TProjectileType] of single = (2, 2, 2, 6);
+  ProjectilePredictJitter:array[TProjectileType] of single = (2, 2, 2, 4);
 
 
 { TKMProjectiles }
