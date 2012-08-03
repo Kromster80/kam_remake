fd6208bb887f2b7b20c15b44eed98b4a30ac882e
Kromster80 <kromster80@gmail.com>
2012-08-03 06:07:09 +0000

    Comment to Lewin
    git-svn-id: svn://localhost/trunk@3707 a386ff7f-d321-c144-bb17-e251d28fee14

--- KM_UnitActionWalkTo.pas
+++ KM_UnitActionWalkTo.pas
@@ -1064,6 +1064,7 @@ begin
       if KMStepIsDiag(fUnit.PrevPosition,fUnit.NextPosition) then IncVertex; //Occupy the vertex
     end else
     begin
+      //@Lewin: Please check these two are unused?
       ThisAction := fUnit.GetUnitAction; //We need to know whether DoUnitInteraction destroys Self (this action)
       U := fUnit; //Could be destroyed by DoUnitInteraction, we need to check afterwards
 
