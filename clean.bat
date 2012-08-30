4c3377bceaaf8a6a111591b9a42fe391c0f557fa
Kromster80 <kromster80@gmail.com>
2012-08-30 13:15:56 +0000

    Updated clean.bat to remove project map file (but keep maps)
    git-svn-id: svn://localhost/trunk@3901 a386ff7f-d321-c144-bb17-e251d28fee14

--- clean.bat
+++ clean.bat
@@ -1,8 +1,9 @@
-rmdir /S /Q __history logs
+rmdir /S /Q __history logs backup
 
 erase /F /Q /S *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S *.o *.or *.ppu *.compiled *.local
-erase /F /Q /S *.tmp *.log thumbs.db *.ion *.skincfg *.identcache *.tvsconfig
+erase /F /Q /S *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig
+
 erase /F /Q /S /A:H *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S /A:H *.o *.or *.ppu *.compiled *.local
-erase /F /Q /S /A:H *.tmp *.log thumbs.db *.ion *.skincfg *.identcache *.tvsconfig
+erase /F /Q /S /A:H *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig
