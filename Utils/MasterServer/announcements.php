13c2cc96a9220b9f90820677d294539cfeccdec7
LewinJH <lewinjh@gmail.com>
2012-10-19 07:11:30 +0000

    - Minor translation fixes emailed to us  - Proposal  - Master server announcement about TeamSpeak (been up for a few days I just hadn't committed it)
    git-svn-id: svn://localhost/trunk@4121 a386ff7f-d321-c144-bb17-e251d28fee14

--- Utils/MasterServer/announcements.php
+++ Utils/MasterServer/announcements.php
@@ -89,7 +89,7 @@ if($Rev != $MAIN_VERSION)
 }
 else
 {
-	echo 'Enjoy playing KaM Remake? Please consider [$00EEFF]donating[] to support future development. Visit [$F8A070]www.kamremake.com/donations[]';
+	echo 'Enjoy playing KaM Remake? Please consider [$00EEFF]donating[] to support future development. Visit [$F8A070]www.kamremake.com/donations[]||Use [$F23A40]TeamSpeak[] to talk with your allies while playing KaM. There are instructions at [$F8A070]www.kamremake.com[]';
 	//echo 'The 2 vs 2 Powah Tournament has begun! Go to [$F8A070]www.kamremake.com[] to find out more!||Can you help translate our website [$F8A070]www.kamremake.com[]? We need volunteers to help translate it into any language. Please contact us through our website.';
 	//echo 'Powah Tour (Florek, Mulberry, To) are organising a tournament! Go to [$F8A070]www.kamremake.com[] to find out more!||Can you help translate our website [$F8A070]www.kamremake.com[]? We need volunteers to help translate it into any language. Please contact us through our website.';
 	//echo '[$FF6EAF]Welcome to the 4th multiplayer demo[] :)|Please report any issues.';
