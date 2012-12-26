3988c7540e23303ea45e60e46d3a974536d9dd89
Kromster80 <kromster80@gmail.com>
2012-12-26 17:20:53 +0000

    Reverted DPR
    git-svn-id: svn://localhost/trunk@4396 a386ff7f-d321-c144-bb17-e251d28fee14

--- KaM_Remake.dpr
+++ KaM_Remake.dpr
@@ -2,7 +2,16 @@ program KaM_Remake;
 {$I KaM_Remake.inc}
 
 uses
+  {$IFDEF USE_MAD_EXCEPT}
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListModules,
+  {$ENDIF}
+  {$IFDEF UNIX} cthreads, {$ENDIF} //Required for thread support on Unix/Linux
+  //{$IFDEF WDC} FastMM4, {$ENDIF} //Can be used only in Delphi, not Lazarus
   Forms,
+  {$IFDEF FPC} Interfaces, {$ENDIF}
   KM_FormMain in 'KM_FormMain.pas' {FormMain},
   KM_FormLoading in 'KM_FormLoading.pas' {FormLoading},
   KM_AI in 'KM_AI.pas',
@@ -24,6 +33,7 @@ uses
   KM_DedicatedServer in 'KM_DedicatedServer.pas',
   KM_Defaults in 'KM_Defaults.pas',
   KM_DeliverQueue in 'KM_DeliverQueue.pas',
+  {$IFDEF USE_MAD_EXCEPT}KM_Exceptions in 'KM_Exceptions.pas',{$ENDIF}
   KM_FogOfWar in 'KM_FogOfWar.pas',
   KM_Houses in 'KM_Houses.pas',
   KM_InterfaceDefaults in 'KM_InterfaceDefaults.pas',
