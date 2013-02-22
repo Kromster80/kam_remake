6b79c6a347ea96b902a9c9e70d77152963a47400
Kromster80 <kromster80@gmail.com>
2013-02-22 19:13:48 +0000

    Improving BugParser: - scan folder in UI - fixed column sorting - export linklist button - code cleanup
    git-svn-id: svn://localhost/trunk@4665 a386ff7f-d321-c144-bb17-e251d28fee14

--- Utils/BugParser/Unit_Form.dfm
+++ Utils/BugParser/Unit_Form.dfm
@@ -7,23 +7,29 @@ object Form1: TForm1
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
-  Font.Height = -11
+  Font.Height = -13
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
   OnCreate = FormCreate
-  OnDestroy = FormDestroy
   DesignSize = (
     633
     337)
   PixelsPerInch = 96
-  TextHeight = 13
+  TextHeight = 16
   object Button1: TButton
-    Left = 280
+    Left = 320
     Top = 304
     Width = 75
     Height = 25
-    Caption = 'Button1'
+    Anchors = [akLeft, akBottom]
+    Caption = 'Scan'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
     TabOrder = 0
     OnClick = Button1Click
   end
@@ -40,5 +46,36 @@ object Form1: TForm1
     Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goRangeSelect, goColSizing, goEditing, goRowSelect, goThumbTracking, goFixedRowClick]
     TabOrder = 1
     OnFixedCellClick = StringGrid1FixedCellClick
+  end
+  object Edit1: TEdit
+    Left = 8
+    Top = 304
+    Width = 305
+    Height = 24
+    Anchors = [akLeft, akBottom]
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 2
+    Text = 'C:\Users\Krom\Desktop\123\'
+  end
+  object Button2: TButton
+    Left = 400
+    Top = 304
+    Width = 113
+    Height = 25
+    Anchors = [akLeft, akBottom]
+    Caption = 'Export links'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 3
+    OnClick = Button2Click
   end
 end
--- Utils/BugParser/Unit_Form.pas
+++ Utils/BugParser/Unit_Form.pas
@@ -7,49 +7,52 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     StringGrid1: TStringGrid;
+    Edit1: TEdit;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
     procedure StringGrid1FixedCellClick(Sender: TObject; ACol, ARow: Integer);
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    fLastSortColumn: Integer;
   public
-    InfoList: TStringList;
-    procedure ScanFolder(aPath: string; aList: TStringList);
+    procedure ScanFolder(aPath: string);
     procedure SortStringGrid(var aStringGrid: TStringGrid; aColumn: Integer);
   end;
 
   TStringArray = array [0..5] of string;
 
+
 var
   Form1: TForm1;
+
 
 implementation
 {$R *.dfm}
 
 
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  Path: string;
+begin
+  Path := Edit1.Text;
+  ScanFolder(Path);
+end;
+
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  StringGrid1.Cols[5].SaveToFile(Edit1.Text + 'url_list2.txt');
+end;
+
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  InfoList := TStringList.Create;
+  fLastSortColumn := -1;
 end;
 
 
-procedure TForm1.FormDestroy(Sender: TObject);
-begin
-  InfoList.Free;
-end;
-
-
-procedure TForm1.Button1Click(Sender: TObject);
-var Path: string;
-begin
-  Path := ExtractFilePath(Application.ExeName);
-  Path := 'C:\Users\Krom\Desktop\123\';
-  ScanFolder(Path, InfoList);
-
-  InfoList.SaveToFile(Path + 'url_list.txt');
-end;
-
-
-procedure TForm1.ScanFolder(aPath: string; aList: TStringList);
+procedure TForm1.ScanFolder(aPath: string);
   function ExtractInfo(aFilename: string): TStringArray;
   var
     S: TStringList;
@@ -66,49 +69,53 @@ procedure TForm1.ScanFolder(aPath: string; aList: TStringList);
     S.LoadFromFile(aFilename);
     SS := S.Text;
 
+    //Date/time
     I := Pos('date/time         : ', SS);
     if I <> 0 then
     begin
       Inc(I, 20);
       K := PosEx(#10, SS, I);
       if K > I then
-        Result[2] := Copy(SS, I, K-I+1);
+        Result[0] := Copy(SS, I, K-I-1);
     end;
 
-    I := Pos('http://kam', SS);
-    if I <> 0 then
-    begin
-      K := PosEx('.zip', SS, I);
-
-      if K > I then
-        Result[0] := Copy(SS, I, K-I+4);
-    end;
-
+    //Exception text
     I := Pos('exception message : ', SS);
     if I <> 0 then
     begin
       Inc(I, 20);
       K := PosEx(#10, SS, I);
       if K > I then
-        Result[1] := Copy(SS, I, K-I+1);
+        Result[1] := Copy(SS, I, K-I-1);
     end;
 
-    I := Pos('contact email     : ', SS);
-    if I <> 0 then
-    begin
-      Inc(I, 20);
-      K := PosEx(#10, SS, I);
-      if K > I then
-        Result[2] := Copy(SS, I, K-I+1);
-    end;
-
+    //Username
     I := Pos('from user:'#13#10, SS);
     if I <> 0 then
     begin
       Inc(I, 12);
       K := PosEx(#10, SS, I);
       if K > I then
-        Result[3] := Copy(SS, I, K-I+1);
+        Result[2] := Copy(SS, I, K-I-1);
+    end;
+
+    //Contact email
+    I := Pos('contact email     : ', SS);
+    if I <> 0 then
+    begin
+      Inc(I, 20);
+      K := PosEx(#10, SS, I);
+      if K > I then
+        Result[3] := Copy(SS, I, K-I-1);
+    end;
+
+    //Link to crashreport
+    I := Pos('http://kam', SS);
+    if I <> 0 then
+    begin
+      K := PosEx('.zip', SS, I);
+      if K > I then
+        Result[4] := Copy(SS, I, K-I+4);
     end;
   end;
 var
@@ -116,21 +123,26 @@ var
   I: Integer;
   StringInfo: TStringArray;
 begin
-  aList.Clear;
-
   if not DirectoryExists(aPath) then Exit;
 
   StringGrid1.ColCount := 10;
-    StringGrid1.ColWidths[0] := 30;
-    StringGrid1.ColWidths[1] := 130;
-    StringGrid1.ColWidths[2] := 230;
+  StringGrid1.ColWidths[0] := 40;
+  StringGrid1.ColWidths[1] := 200;
+  StringGrid1.ColWidths[2] := 300;
+  StringGrid1.ColWidths[3] := 300;
+  StringGrid1.ColWidths[4] := 200;
+  StringGrid1.ColWidths[5] := 350;
+
+  StringGrid1.Cols[0].Text := '#';
+  StringGrid1.Cols[1].Text := 'Timestamp';
+  StringGrid1.Cols[2].Text := 'Exception';
+  StringGrid1.Cols[3].Text := 'Description';
+  StringGrid1.Cols[4].Text := 'Contact';
+  StringGrid1.Cols[5].Text := 'Link';
 
   FindFirst(aPath + '*.eml', faAnyFile - faDirectory, SearchRec);
   repeat
-
     StringInfo := ExtractInfo(aPath + SearchRec.Name);
-
-    aList.Add(StringInfo[0]);
 
     StringGrid1.RowCount := StringGrid1.RowCount + 1;
     StringGrid1.Cells[0, StringGrid1.RowCount-1] := IntToStr(StringGrid1.RowCount-1);
@@ -145,32 +157,47 @@ end;
 
 procedure TForm1.SortStringGrid(var aStringGrid: TStringGrid; aColumn: Integer);
 const
-  TheSeparator = '^';
+  TheSeparator = '^`';
 var
-  RCount, I, J, K, ThePosition: integer;
+  RCount, I, ThePosition: Integer;
   MyList: TStringList;
   MyString, TempString: string;
 begin
   RCount := aStringGrid.RowCount;
+
   MyList := TStringList.Create;
   MyList.Sorted := False;
   try
-      for I := 1 to (RCount - 1) do
-        MyList.Add(aStringGrid.Rows[I].Strings[aColumn] + TheSeparator +
-          aStringGrid.Rows[I].Text);
-      Mylist.Sort;
-      for K := 1 to Mylist.Count do
-      begin
-        MyString := MyList.Strings[(K - 1)];
-        ThePosition := Pos(TheSeparator, MyString);
-        TempString  := '';
-        {Eliminate the Text of the column on which we have sorted the StringGrid}
-        TempString := Copy(MyString, (ThePosition + 1), Length(MyString));
-        MyList.Strings[(K - 1)] := '';
-        MyList.Strings[(K - 1)] := TempString;
-      end;
-      for J := 1 to (RCount - 1) do
-        aStringGrid.Rows[J].Text := MyList.Strings[(J - 1)];
+    for I := 1 to RCount - 1 do
+      MyList.Add(aStringGrid.Rows[I].Strings[aColumn] + TheSeparator + aStringGrid.Rows[I].Text);
+
+    Mylist.Sort;
+
+    //Trim sort key
+    for I := 0 to Mylist.Count - 1 do
+    begin
+      MyString := MyList.Strings[I];
+      ThePosition := Pos(TheSeparator, MyString);
+      //Eliminate the Text of the column on which we have sorted the StringGrid
+      TempString := Copy(MyString, ThePosition + Length(TheSeparator), Length(MyString));
+      MyList.Strings[I] := TempString;
+    end;
+
+    if fLastSortColumn = aColumn then
+    begin
+      for I := 1 to RCount - 1 do
+        aStringGrid.Rows[I].Text := MyList.Strings[RCount - I-1];
+      //Reset column so next time we use straight sort direction
+      fLastSortColumn := -1;
+    end
+    else
+    begin
+      for I := 1 to RCount - 1 do
+        aStringGrid.Rows[I].Text := MyList.Strings[I-1];
+      //Remember column so next time we invert sort direction
+      fLastSortColumn := aColumn;
+    end;
+
   finally
     MyList.Free;
   end;
@@ -181,5 +208,6 @@ procedure TForm1.StringGrid1FixedCellClick(Sender: TObject; ACol, ARow: Integer)
 begin
   SortStringGrid(StringGrid1, aCol);
 end;
+
 
 end.
