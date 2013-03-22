d0bd96f982dae8a6ad191faa4e514deb2a624f3e
Kromster80 <kromster80@gmail.com>
2013-03-22 18:48:14 +0000

    Version for mapmakers test
    git-svn-id: svn://localhost/trunk@4876 a386ff7f-d321-c144-bb17-e251d28fee14

--- KM_Defaults.pas
+++ KM_Defaults.pas
@@ -27,8 +27,8 @@ const
   MENU_DESIGN_X         = 1024;         //Thats the size menu was designed for. All elements are placed in this size
   MENU_DESIGN_Y         = 768;          //Thats the size menu was designed for. All elements are placed in this size
 
-  GAME_REVISION         = 'r4785';       //Should be updated for every release (each time save format is changed)
-  GAME_VERSION          = 'Next Demo ' + GAME_REVISION;       //Game version string displayed in menu corner
+  GAME_REVISION         = 'r4876';       //Should be updated for every release (each time save format is changed)
+  GAME_VERSION          = 'Scripting Demo ' + GAME_REVISION;       //Game version string displayed in menu corner
   NET_PROTOCOL_REVISON  = GAME_REVISION;     //Clients of this version may connect to the dedicated server
 
   SETTINGS_FILE         = 'KaM_Remake_Settings.ini';
