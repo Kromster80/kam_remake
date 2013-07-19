2375c7aea64b0b5b640b4a96f37b1337f9ccde52
Kromster80 <kromster80@gmail.com>
2013-07-19 06:32:50 +0000

    Forgot 2 files
    git-svn-id: svn://localhost/trunk@5630 a386ff7f-d321-c144-bb17-e251d28fee14

---
+++ KM_GUIMenuError.pas
@@ -0,0 +1,63 @@
+unit KM_GUIMenuError;
+{$I KaM_Remake.inc}
+interface
+uses
+  Controls, Math, SysUtils,
+  KM_Controls, KM_InterfaceDefaults;
+
+
+type
+  TKMGUIMenuError = class
+  private
+    fOnPageChange: TGUIEventText;
+
+    procedure BackClick(Sender: TObject);
+  protected
+    Panel_Error: TKMPanel;
+    Label_Error: TKMLabel;
+    Button_ErrorBack: TKMButton;
+  public
+    constructor Create(aParent: TKMPanel; aOnPageChange: TGUIEventText);
+
+    procedure Show(aText: UnicodeString);
+  end;
+
+
+implementation
+uses KM_ResTexts, KM_RenderUI, KM_ResFonts;
+
+
+{ TKMGUIMenuError }
+constructor TKMGUIMenuError.Create(aParent: TKMPanel; aOnPageChange: TGUIEventText);
+begin
+  inherited Create;
+
+  fOnPageChange := aOnPageChange;
+
+  Panel_Error := TKMPanel.Create(aParent, 0, 0, aParent.Width, aParent.Height);
+  Panel_Error.Stretch;
+    with TKMLabel.Create(Panel_Error, aParent.Width div 2, aParent.Height div 2 - 20, gResTexts[TX_MENU_ERROR], fnt_Antiqua, taCenter) do
+      Center;
+    Label_Error := TKMLabel.Create(Panel_Error, 8, aParent.Height div 2+10, aParent.Width-16, 200, '...', fnt_Grey, taCenter);
+    Label_Error.Center;
+    Label_Error.AutoWrap := True;
+    Button_ErrorBack := TKMButton.Create(Panel_Error,100,630,224,30,gResTexts[TX_MENU_BACK],bsMenu);
+    Button_ErrorBack.Center;
+    Button_ErrorBack.OnClick := BackClick;
+end;
+
+
+procedure TKMGUIMenuError.BackClick(Sender: TObject);
+begin
+  fOnPageChange(gpMainMenu);
+end;
+
+
+procedure TKMGUIMenuError.Show(aText: UnicodeString);
+begin
+  Label_Error.Caption := aText;
+  Panel_Error.Show;
+end;
+
+
+end.
---
+++ KM_GUIMenuLoading.pas
@@ -0,0 +1,57 @@
+unit KM_GUIMenuLoading;
+{$I KaM_Remake.inc}
+interface
+uses
+  Controls, Math, SysUtils,
+  KM_Controls, KM_InterfaceDefaults;
+
+
+type
+  TKMGUIMenuLoading = class
+  private
+    fOnPageChange: TGUIEventText;
+  protected
+    Panel_Loading: TKMPanel;
+    Label_Loading: TKMLabel;
+  public
+    constructor Create(aParent: TKMPanel; aOnPageChange: TGUIEventText);
+
+    procedure AppendText(const aText: UnicodeString);
+    procedure Show(aText: UnicodeString);
+  end;
+
+
+implementation
+uses KM_ResTexts, KM_RenderUI, KM_ResFonts;
+
+
+{ TKMGUIMenuLoading }
+constructor TKMGUIMenuLoading.Create(aParent: TKMPanel; aOnPageChange: TGUIEventText);
+begin
+  inherited Create;
+
+  fOnPageChange := aOnPageChange;
+
+  Panel_Loading := TKMPanel.Create(aParent,0,0,aParent.Width, aParent.Height);
+  Panel_Loading.Stretch;
+    with TKMLabel.Create(Panel_Loading, aParent.Width div 2, aParent.Height div 2 - 20, gResTexts[TX_MENU_LOADING], fnt_Outline, taCenter) do
+      Center;
+    Label_Loading := TKMLabel.Create(Panel_Loading, aParent.Width div 2, aParent.Height div 2+10, '...', fnt_Grey, taCenter);
+    Label_Loading.Center;
+end;
+
+
+procedure TKMGUIMenuLoading.AppendText(const aText: UnicodeString);
+begin
+  Label_Loading.Caption := Label_Loading.Caption + aText + '|';
+end;
+
+
+procedure TKMGUIMenuLoading.Show(aText: UnicodeString);
+begin
+  Label_Loading.Caption := aText;
+  Panel_Loading.Show;
+end;
+
+
+end.
