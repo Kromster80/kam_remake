48187acce96ecc1de67e7cd4d13c0cfb0e778360
Kromster80 <kromster80@gmail.com>
2014-04-15 10:11:54 +0000

    Clean deletes more junk
    git-svn-id: svn://localhost/trunk@6185 a386ff7f-d321-c144-bb17-e251d28fee14

--- clean.bat
+++ clean.bat
@@ -1,9 +1,13 @@
-rmdir /S /Q __history logs backup dcu
+;Delete folders recursively
+FOR /D /R %%X IN (__history) DO RD /S /Q "%%X"
+FOR /D /R %%X IN (backup) DO RD /S /Q "%%X"
+
+rmdir /S /Q logs dcu
 
 erase /F /Q /S *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S *.o *.or *.ppu *.compiled *.local
-erase /F /Q /S *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig *.mi
+erase /F /Q /S *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig *.mi *.stat
 
 erase /F /Q /S /A:H *.~* *.ddp *.drc *.dcp *.dcu
 erase /F /Q /S /A:H *.o *.or *.ppu *.compiled *.local
-erase /F /Q /S /A:H *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig *.mi
+erase /F /Q /S /A:H *.tmp *.log thumbs.db KaM_Remake.map descript.ion *.skincfg *.identcache *.tvsconfig *.mi *.stat
