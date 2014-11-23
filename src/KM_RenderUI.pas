Kromster80/kam_remake

commit eab4b51145e2ecc1d3e2a9d2be2d14ba35f13d64

Asian fonts don't load by default for non-Asian locales
git-svn-id: svn://localhost/trunk@6636 a386ff7f-d321-c144-bb17-e251d28fee14

src/KM_RenderUI.pas
@@ -428,7 +428,7 @@ end;
 class procedure TKMRenderUI.WriteText(aLeft, aTop, aWidth: SmallInt; aText: UnicodeString; aFont: TKMFont; aAlign: TKMTextAlign; aColor: TColor4 = $FFFFFFFF; aIgnoreMarkup: Boolean = False; aShowMarkup: Boolean = False);
 var
   I, K: Integer;
-  LineCount,AdvX,AdvY,LineHeight,BlockWidth: Integer;
+  LineCount,AdvX,AdvY,LineHeight,BlockWidth,PrevAtlas: Integer;
   LineWidth: array of Integer; //Use signed format since some fonts may have negative CharSpacing
   FontData: TKMFontData;
   Let: TKMLetter;
@@ -489,11 +489,7 @@ begin
 
   for I := 1 to Length(aText) do
   begin
-    if aText[I] <> #124 then
-      if aText[I] = #32 then
-        Inc(LineWidth[LineCount], FontData.WordSpacing)
-      else
-        Inc(LineWidth[LineCount], FontData.Letters[Ord(aText[I])].Width + FontData.CharSpacing);
+    Inc(LineWidth[LineCount], gRes.Fonts.GetCharWidth(aText[I], aFont));
 
     //If EOL or aText end
     if (aText[I] = #124) or (I = Length(aText)) then
@@ -522,25 +518,9 @@ begin
   glColor4ubv(@aColor);
 
   K := 0;
+  PrevAtlas := -1;
   for I := 1 to Length(aText) do
   begin
-    if (I = 1)
-    or (FontData.Letters[Ord(aText[I-1])].AtlasId <> FontData.Letters[Ord(aText[I])].AtlasId) then
-    begin
-      glBindTexture(GL_TEXTURE_2D, FontData.TexID[FontData.Letters[Ord(aText[I])].AtlasId]);
-      glBegin(GL_QUADS);
-    end;
-
-    //Loop as there might be adjoined tags on same position
-    while (K < Length(Colors)) and (I = Colors[K].FirstChar) do
-    begin
-      if Colors[K].Color = 0 then
-        glColor4ubv(@aColor)
-      else
-        glColor4ubv(@Colors[K].Color);
-      Inc(K);
-    end;
-
     case aText[I] of
       #32:  Inc(AdvX, FontData.WordSpacing);
       #124: begin
@@ -555,23 +535,38 @@ begin
               end;
             end;
       else  begin
-              //Show missing letters in font as "?"
-              if FontData.Used[Ord(aText[I])] <> 0 then
-                Let := FontData.Letters[Ord(aText[I])]
-              else
-                Let := FontData.Letters[Ord('?')];
+              Let := FontData.GetLetter(aText[I]);
+
+              if (I = 1)
+              or (PrevAtlas <> Let.AtlasId) then
+              begin
+                if PrevAtlas <> -1 then
+                  glEnd;
+                PrevAtlas := Let.AtlasId;
+                glBindTexture(GL_TEXTURE_2D, FontData.TexID[Let.AtlasId]);
+                glBegin(GL_QUADS);
+              end;
+
+              //Loop as there might be adjoined tags on same position
+              while (K < Length(Colors)) and (I = Colors[K].FirstChar) do
+              begin
+                if Colors[K].Color = 0 then
+                  glColor4ubv(@aColor)
+                else
+                  glColor4ubv(@Colors[K].Color);
+                Inc(K);
+              end;
 
               glTexCoord2f(Let.u1, Let.v1); glVertex2f(AdvX            , AdvY            + Let.YOffset);
               glTexCoord2f(Let.u2, Let.v1); glVertex2f(AdvX + Let.Width, AdvY            + Let.YOffset);
               glTexCoord2f(Let.u2, Let.v2); glVertex2f(AdvX + Let.Width, AdvY+Let.Height + Let.YOffset);
               glTexCoord2f(Let.u1, Let.v2); glVertex2f(AdvX            , AdvY+Let.Height + Let.YOffset);
               Inc(AdvX, Let.Width + FontData.CharSpacing);
+
+              if (I = Length(aText)) then
+                glEnd;
             end;
     end;
-
-    if (I = Length(aText))
-    or (FontData.Letters[Ord(aText[I])].AtlasId <> FontData.Letters[Ord(aText[I+1])].AtlasId) then
-      glEnd;
   end;
   glBindTexture(GL_TEXTURE_2D, 0);
 

src/KM_Defaults.pas
@@ -62,7 +62,6 @@ var
   HUNGARIAN_GROUP_ORDER :Boolean = True; //Use Hungarian algorithm to reorder warrior groups when walking
   AI_GEN_NAVMESH        :Boolean = True; //Generate navmesh for AI to plan attacks/defenses
   AI_GEN_INFLUENCE_MAPS :Boolean = True; //Generate influence maps for AI to plan attacks/defenses
-  THREADED_FONT_LOAD    :Boolean = True; //Load each font in separate thread to improve load times
   //Not fully implemented yet
   USE_CCL_WALKCONNECT   :Boolean = False; //Use CCL instead of FloodFill for walk-connect (CCL is generaly worse. It's a bit slower, counts 1 tile areas and needs more AreaIDs to work / makes sparsed IDs)
   DYNAMIC_FOG_OF_WAR    :Boolean = False; //Whenever dynamic fog of war is enabled or not

src/KM_GameApp.pas
@@ -108,7 +108,7 @@ begin
   fRender := TRender.Create(aRenderControl, aScreenX, aScreenY, aVSync);
 
   gRes := TKMResource.Create(aOnLoadingStep, aOnLoadingText);
-  gRes.LoadMainResources(fGameSettings.Locale);
+  gRes.LoadMainResources(fGameSettings.Locale, fGameSettings.LoadFullFonts);
 
   {$IFDEF USE_MAD_EXCEPT}fExceptions.LoadTranslation;{$ENDIF}
 
@@ -211,6 +211,8 @@ begin
 
   //Recreate resources that use Locale info
   gRes.LoadLocaleResources(fGameSettings.Locale);
+  //Fonts might need reloading too
+  gRes.LoadLocaleFonts(fGameSettings.Locale, fGameSettings.LoadFullFonts);
 
   {$IFDEF USE_MAD_EXCEPT}fExceptions.LoadTranslation;{$ENDIF}
 

src/KM_Settings.pas
@@ -43,6 +43,7 @@ type
     fBrightness: Byte;
     fScrollSpeed: Byte;
     fAlphaShadows: Boolean;
+    fLoadFullFonts: Boolean;
     fLocale: AnsiString;
     fMusicOff: Boolean;
     fShuffleOn: Boolean;
@@ -71,6 +72,7 @@ type
     procedure SetBrightness(aValue: Byte);
     procedure SetScrollSpeed(aValue: Byte);
     procedure SetAlphaShadows(aValue: Boolean);
+    procedure SetLoadFullFonts(aValue: Boolean);
     procedure SetLocale(aLocale: AnsiString);
     procedure SetMusicOff(aValue: Boolean);
     procedure SetShuffleOn(aValue: Boolean);
@@ -105,6 +107,7 @@ type
     property Brightness: Byte read fBrightness write SetBrightness;
     property ScrollSpeed: Byte read fScrollSpeed write SetScrollSpeed;
     property AlphaShadows: Boolean read fAlphaShadows write SetAlphaShadows;
+    property LoadFullFonts: Boolean read fLoadFullFonts write SetLoadFullFonts;
     property Locale: AnsiString read fLocale write SetLocale;
     property MusicOff: Boolean read fMusicOff write SetMusicOff;
     property ShuffleOn: Boolean read fShuffleOn write SetShuffleOn;
@@ -278,6 +281,7 @@ begin
   try
     fBrightness       := F.ReadInteger('GFX', 'Brightness',       1);
     fAlphaShadows     := F.ReadBool   ('GFX', 'AlphaShadows',     True);
+    fLoadFullFonts    := F.ReadBool   ('GFX', 'LoadFullFonts',    False);
 
     fAutosave       := F.ReadBool   ('Game', 'Autosave',       True); //Should be ON by default
     fScrollSpeed    := F.ReadInteger('Game', 'ScrollSpeed',    10);
@@ -331,6 +335,7 @@ begin
   try
     F.WriteInteger('GFX','Brightness',    fBrightness);
     F.WriteBool   ('GFX','AlphaShadows',  fAlphaShadows);
+    F.WriteBool   ('GFX','LoadFullFonts', fLoadFullFonts);
 
     F.WriteBool   ('Game','Autosave',     fAutosave);
     F.WriteInteger('Game','ScrollSpeed',  fScrollSpeed);
@@ -413,6 +418,13 @@ end;
 procedure TGameSettings.SetAlphaShadows(aValue: Boolean);
 begin
   fAlphaShadows := aValue;
+  Changed;
+end;
+
+
+procedure TGameSettings.SetLoadFullFonts(aValue: Boolean);
+begin
+  fLoadFullFonts := aValue;
   Changed;
 end;
 

src/res/KM_ResFonts.pas
@@ -11,6 +11,8 @@ uses
 type
   TKMFont = (fnt_Antiqua, fnt_Game, fnt_Grey,
     fnt_Metal, fnt_Mini, fnt_Outline, fnt_Arial);
+
+  TKMFontLoadLevel = (fll_Full, fll_Minimal);
   {
   Removed fonts that were in KaM:
   Adam (unused)
@@ -61,13 +63,14 @@ type
     Letters: array [0..High(Word)] of TKMLetter;
 
     procedure LoadFont(const aFileName: string; aPal: TKMPalData);
-    procedure LoadFontX(const aFileName: string);
+    procedure LoadFontX(const aFileName: string; aLoadLevel: TKMFontLoadLevel = fll_Full);
     procedure GenerateTextures(aTexMode: TTexFormat);
     procedure Compact;
     procedure ExportAtlasBmp(aBitmap: TBitmap; aIndex: Integer; aShowCells: Boolean); overload;
     procedure ExportAtlasBmp(const aPath: string; aIndex: Integer); overload;
     procedure ExportAtlasPng(const aFilename: string; aIndex: Integer);
 
+    function GetLetter(aChar: WideChar): TKMLetter;
     property AtlasCount: Byte read fAtlasCount;
     property TexID[aIndex: Integer]: Cardinal read GetTexID;
 
@@ -79,23 +82,10 @@ type
   end;
 
 
-  //We can speed up font loading with threads. A large part of loading time is reading
-  //files from disk and decompressing with ZLIB. These tasks are perfect for parallelizing.
-  //Generating OpenGL textures must be done in main thread, however that's <50% of font loading times.
-  //This is a very simple thread which loads a font then exits
-  TKMFontLoadThread = class(TThread)
-    private
-      fFontData: TKMFontData;
-      fPath: UnicodeString;
-    public
-      constructor Create(aFontData: TKMFontData; aPath: UnicodeString);
-      procedure Execute; override;
-  end;
-
-
   //Collection of fonts
   TKMResourceFont = class
   private
+    fLoadLevel: TKMFontLoadLevel;
     fFontData: array [TKMFont] of TKMFontData;
     function GetFontData(aIndex: TKMFont): TKMFontData;
   public
@@ -103,6 +93,7 @@ type
     destructor Destroy; override;
 
     property FontData[aIndex: TKMFont]: TKMFontData read GetFontData;
+    property LoadLevel: TKMFontLoadLevel read fLoadLevel;
     class function GuessPalette(aFileName: string): TKMPal;
 
     function GetCharWidth(aChar: WideChar; aFont: TKMFont): Integer;
@@ -110,12 +101,14 @@ type
     function CharsThatFit(const aText: UnicodeString; aFont: TKMFont; aMaxPxWidth: integer): integer;
     function GetTextSize(const aText: UnicodeString; Fnt: TKMFont; aCountMarkup: Boolean = False): TKMPoint;
 
-    procedure LoadFonts;
+    procedure LoadFonts(aLoadLevel: TKMFontLoadLevel = fll_Full);
     procedure ExportFonts;
   end;
 
 
 const
+  PLACEHOLDER_CHAR = 0; //Box, used for characters missing from font
+
   FontInfo: array [TKMFont] of TKMFontInfo = (
     (FontFile: 'antiqua';     Pal: pal_0;         TexMode: tf_RGB5A1),
     (FontFile: 'game';        Pal: pal_bw;        TexMode: tf_Alpha8),
@@ -227,7 +220,7 @@ begin
 end;
 
 
-procedure TKMFontData.LoadFontX(const aFileName: string);
+procedure TKMFontData.LoadFontX(const aFileName: string; aLoadLevel: TKMFontLoadLevel = fll_Full);
 const
   FNTX_HEAD: AnsiString = 'FNTX';
 var
@@ -263,6 +256,14 @@ begin
     DecompressionStream.Read(fAtlasCount, 1);
     DecompressionStream.Read(fTexSizeX, 2);
     DecompressionStream.Read(fTexSizeY, 2);
+
+    if aLoadLevel = fll_Minimal then
+    begin
+      fAtlasCount := 1; //Only load the first atlas
+      for I := 0 to High(Word) do
+        Used[I] := Byte(Letters[I].AtlasId = 0); //Only allow letters on first atlas
+    end;
+
     SetLength(fAtlases, fAtlasCount);
     for I := 0 to fAtlasCount - 1 do
     begin
@@ -294,12 +295,30 @@ end;
 procedure TKMFontData.GenerateTextures(aTexMode: TTexFormat);
 var
   I: Integer;
+  TextureRAM: Cardinal;
 begin
+  TextureRAM := 0;
   for I := 0 to fAtlasCount - 1 do
-  if Length(fAtlases[I].TexData) <> 0 then
-    fAtlases[I].TexID := TRender.GenTexture(fTexSizeX, fTexSizeY, @fAtlases[I].TexData[0], aTexMode)
+    if fAtlases[I].TexID = 0 then //Don't load atlases twice if switching from minimal to full
+      if Length(fAtlases[I].TexData) <> 0 then
+      begin
+        fAtlases[I].TexID := TRender.GenTexture(fTexSizeX, fTexSizeY, @fAtlases[I].TexData[0], aTexMode);
+        Inc(TextureRAM, fTexSizeX * fTexSizeY * TexFormatSize[aTexMode]);
+      end
+      else
+        fAtlases[I].TexID := 0;
+
+  if LOG_EXTRA_GFX then
+    gLog.AddNoTime( 'Font RAM usage: '+IntToStr(TextureRAM));
+end;
+
+
+function TKMFontData.GetLetter(aChar: WideChar): TKMLetter;
+begin
+  if Used[Ord(aChar)] <> 0 then
+    Result := Letters[Ord(aChar)]
   else
-    fAtlases[I].TexID := 0;
+    Result := Letters[PLACEHOLDER_CHAR];
 end;
 
 
@@ -447,55 +466,21 @@ begin
 end;
 
 
-procedure TKMResourceFont.LoadFonts;
+procedure TKMResourceFont.LoadFonts(aLoadLevel: TKMFontLoadLevel = fll_Full);
 var
   F: TKMFont;
   FntPath: string;
-  LoaderThreads: array[TKMFont] of TKMFontLoadThread;
-  Done, ProcessedFont: Boolean;
   StartTime, TotalTime: Cardinal;
 begin
+  fLoadLevel := aLoadLevel;
   StartTime := TimeGet;
 
   for F := Low(TKMFont) to High(TKMFont) do
   begin
     FntPath := ExeDir + FONTS_FOLDER + FontInfo[F].FontFile + '.fntx';
-
-    if THREADED_FONT_LOAD then
-      LoaderThreads[F] := TKMFontLoadThread.Create(fFontData[F], FntPath)
-    else
-    begin
-      fFontData[F].LoadFontX(FntPath);
-      fFontData[F].GenerateTextures(FontInfo[F].TexMode);
-      fFontData[F].Compact;
-    end;
-  end;
-
-  if THREADED_FONT_LOAD then
-  begin
-    //Wait for child threads to finish, then generate textures (can only use OGL from main thread)
-    Done := False;
-    while not Done do
-    begin
-      Done := True;
-      ProcessedFont := False;
-      for F := Low(TKMFont) to High(TKMFont) do
-        if LoaderThreads[F] <> nil then //nil threads are ones we already processed
-          if LoaderThreads[F].ReturnValue = 1 then //.Finished doesn't exist in FPC, so use ReturnValue we set
-          begin
-            FreeAndNil(LoaderThreads[F]);
-            fFontData[F].GenerateTextures(FontInfo[F].TexMode);
-            fFontData[F].Compact;
-            ProcessedFont := True;
-          end
-          else
-            Done := False;
-      //If we didn't find any finished threads this pass, don't hog CPU with main thread
-      //(gives children more time on CPU). If we found a finished thread, keep on checking
-      //because other threads probably finished while we were generating textures
-      if not ProcessedFont then
-        Sleep(10);
-    end;
+    fFontData[F].LoadFontX(FntPath, aLoadLevel);
+    fFontData[F].GenerateTextures(FontInfo[F].TexMode);
+    fFontData[F].Compact;
   end;
 
   TotalTime := GetTimeSince(StartTime);
@@ -529,11 +514,7 @@ begin
     if aChar = #32 then
       Result := fFontData[aFont].WordSpacing
     else
-      if fFontData[aFont].Used[Ord(aChar)] <> 0 then
-        Result := fFontData[aFont].Letters[Ord(aChar)].Width + fFontData[aFont].CharSpacing
-      else
-        //Missing characters get rendered as ? so we should count their width as that too
-        Result := fFontData[aFont].Letters[Ord('?')].Width + fFontData[aFont].CharSpacing;
+      Result := fFontData[aFont].GetLetter(aChar).Width + fFontData[aFont].CharSpacing;
 end;
 
 
@@ -690,24 +671,6 @@ begin
   Result.Y := (fFontData[Fnt].BaseHeight + fFontData[Fnt].LineSpacing) * LineCount;
   for I := 1 to LineCount do
     Result.X := Math.max(Result.X, LineWidth[I]);
-end;
-
-
-{ TKMFontLoadThread }
-constructor TKMFontLoadThread.Create(aFontData: TKMFontData; aPath: UnicodeString);
-begin
-  inherited Create(False);
-  FreeOnTerminate := False;
-  fFontData := aFontData;
-  fPath := aPath;
-end;
-
-
-procedure TKMFontLoadThread.Execute;
-begin
-  inherited;
-  fFontData.LoadFontX(fPath);
-  ReturnValue := 1; //Signals that we have finished
 end;
 
 

src/res/KM_ResLocales.pas
@@ -11,6 +11,7 @@ type
     Code: AnsiString; //3-letter code: 'eng', 'rus'
     Title: string; //Full name: 'English', 'Russian'
     FontCodepage: Word;
+    NeedsFullFonts: Boolean;
     FlagSpriteID: Integer;
     FallbackLocale: AnsiString;
     TranslatorCredit: UnicodeString;
@@ -100,9 +101,10 @@ begin
       1: aLocale.Code             := AnsiString(Chunk);
       2: aLocale.Title            := Chunk;
       3: aLocale.FontCodepage     := StrToIntDef(Chunk, 0);
-      4: aLocale.FlagSpriteID     := StrToIntDef(Chunk, 0);
-      5: aLocale.FallbackLocale   := AnsiString(Chunk);
-      6: aLocale.TranslatorCredit := Chunk;
+      4: aLocale.NeedsFullFonts   := Boolean(StrToIntDef(Chunk, 0));
+      5: aLocale.FlagSpriteID     := StrToIntDef(Chunk, 0);
+      6: aLocale.FallbackLocale   := AnsiString(Chunk);
+      7: aLocale.TranslatorCredit := Chunk;
     end;
 
     L := R + 1;

src/res/KM_Resource.pas
@@ -47,9 +47,10 @@ type
 
     function GetDATCRC: Cardinal;
 
-    procedure LoadMainResources(aLocale: AnsiString = '');
+    procedure LoadMainResources(aLocale: AnsiString = ''; aLoadFullFonts: Boolean = True);
     procedure LoadLocaleResources(aLocale: AnsiString = '');
     procedure LoadGameResources(aAlphaShadows: boolean);
+    procedure LoadLocaleFonts(aLocale: AnsiString; aLoadFullFonts: Boolean);
 
     property DataState: TResourceLoadState read fDataState;
     property Cursors: TKMCursors read fCursors;
@@ -131,7 +132,7 @@ begin
 end;
 
 
-procedure TKMResource.LoadMainResources(aLocale: AnsiString = '');
+procedure TKMResource.LoadMainResources(aLocale: AnsiString = ''; aLoadFullFonts: Boolean = True);
 begin
   StepCaption('Reading palettes ...');
   fPalettes := TKMPalettes.Create;
@@ -145,9 +146,15 @@ begin
   fCursors.MakeCursors(fSprites[rxGui]);
   fCursors.Cursor := kmc_Default;
 
+  // Locale info is needed for DAT export and font loading
+  LoadLocaleResources(aLocale);
+
   StepCaption('Reading fonts ...');
   fFonts := TKMResourceFont.Create;
-  fFonts.LoadFonts;
+  if aLoadFullFonts or gResLocales.LocaleByCode(aLocale).NeedsFullFonts then
+    fFonts.LoadFonts(fll_Full)
+  else
+    fFonts.LoadFonts(fll_Minimal);
   gLog.AddTime('Read fonts is done');
 
   fTileset := TKMTileset.Create(ExeDir + 'data'+PathDelim+'defines'+PathDelim+'pattern.dat');
@@ -158,13 +165,9 @@ begin
 
   fSprites.ClearTemp;
 
-  // Locale info is needed for DAT export
-  LoadLocaleResources(aLocale);
-
   fWares := TKMWaresList.Create;
   fHouseDat := TKMHouseDatCollection.Create;
   fUnitDat := TKMUnitDatCollection.Create;
-
 
   StepRefresh;
   gLog.AddTime('ReadGFX is done');
@@ -186,6 +189,15 @@ begin
 
   fSounds := TKMResSounds.Create(gResLocales.UserLocale, gResLocales.FallbackLocale, gResLocales.DefaultLocale);
 end;
+
+
+procedure TKMResource.LoadLocaleFonts(aLocale: AnsiString; aLoadFullFonts: Boolean);
+begin
+  if (Fonts.LoadLevel <> fll_Full)
+  and (aLoadFullFonts or gResLocales.LocaleByCode(aLocale).NeedsFullFonts) then
+    Fonts.LoadFonts(fll_Full);
+end;
+
 
 
 procedure TKMResource.LoadGameResources(aAlphaShadows: Boolean);

commit b0a08e74f0c36bea9650fcbca2fff33ca0f70966

Fixed a bug I introduced in r6636: Color in text is not always working as expected
git-svn-id: svn://localhost/trunk@6656 a386ff7f-d321-c144-bb17-e251d28fee14

src/KM_RenderUI.pas
@@ -521,6 +521,16 @@ begin
   PrevAtlas := -1;
   for I := 1 to Length(aText) do
   begin
+    //Loop as there might be adjoined tags on same position
+    while (K < Length(Colors)) and (I = Colors[K].FirstChar) do
+    begin
+      if Colors[K].Color = 0 then
+        glColor4ubv(@aColor)
+      else
+        glColor4ubv(@Colors[K].Color);
+      Inc(K);
+    end;
+
     case aText[I] of
       #32:  Inc(AdvX, FontData.WordSpacing);
       #124: begin
@@ -544,16 +554,6 @@ begin
                 PrevAtlas := Let.AtlasId;
                 glBindTexture(GL_TEXTURE_2D, FontData.TexID[Let.AtlasId]);
                 glBegin(GL_QUADS);
-              end;
-
-              //Loop as there might be adjoined tags on same position
-              while (K < Length(Colors)) and (I = Colors[K].FirstChar) do
-              begin
-                if Colors[K].Color = 0 then
-                  glColor4ubv(@aColor)
-                else
-                  glColor4ubv(@Colors[K].Color);
-                Inc(K);
               end;
 
               glTexCoord2f(Let.u1, Let.v1); glVertex2f(AdvX            , AdvY            + Let.YOffset);