123ce80f697b76e91604ac4c88405f5220689fd3
thibmo <thibmorozier@live.nl>
2015-04-20 17:41:15 +0200

    Implemented changed requested by Krom. * Looks more compact and tidy now * No extra variables anymore (Ident1 and Ident2 have been removed and were replaced by static values.) * AddHouseSupplySprite(Id); is now used at the very end only, makes more sense.

--- src/KM_RenderPool.pas
+++ src/KM_RenderPool.pas
@@ -664,7 +664,7 @@ end;
 
 procedure TRenderPool.AddHouseSupply(aHouse: THouseType; Loc: TKMPoint; const R1, R2: array of Byte);
 var
-  Id,I,K,Ident1,Ident2: Integer;
+  Id,I,K: Integer;
   R: TRXData;
 
   procedure AddHouseSupplySprite(aId: Integer);
@@ -685,31 +685,17 @@ begin
   for I := 1 to 4 do
   if (R1[I - 1]) > 0 then
   begin
-    // Begin temp fix for input sprites of Armor smithy
-    if aHouse = ht_ArmorSmithy then
+    if aHouse = ht_ArmorSmithy then // For some reason KaM stores these wares in swapped order, here we fix it
     begin
       if I = 1 then // If sprite 1 gets called, render house sprite 2 (Coal)
-      begin
-        Ident1 := 2;
-        Id := gRes.HouseDat[aHouse].SupplyIn[Ident1, Min(R1[I - 1], 5)] + 1;
-        AddHouseSupplySprite(Id);
-      end else
-      if I = 2 then // If sprite 2 gets called, render house sprite 1 (Steel)
-      begin
-        Ident1 := 1;
-        Id := gRes.HouseDat[aHouse].SupplyIn[Ident1, Min(R1[I - 1], 5)] + 1;
-        AddHouseSupplySprite(Id);
-      end else // Back to normal processing for remaining two entries
-      begin
+        Id := gRes.HouseDat[aHouse].SupplyIn[2, Min(R1[I - 1], 5)] + 1
+      else if I = 2 then // If sprite 2 gets called, render house sprite 1 (Steel)
+        Id := gRes.HouseDat[aHouse].SupplyIn[1, Min(R1[I - 1], 5)] + 1
+      else // Back to normal processing for remaining two entries
         Id := gRes.HouseDat[aHouse].SupplyIn[I, Min(R1[I - 1], 5)] + 1;
-        AddHouseSupplySprite(Id);
-      end
-    end else
-    // End temp fix for input sprites of Armor smithy
-    begin
+    end else // End of fix
       Id := gRes.HouseDat[aHouse].SupplyIn[I, Min(R1[I - 1], 5)] + 1;
-      AddHouseSupplySprite(Id);
-    end;
+    AddHouseSupplySprite(Id);
   end;
 
   for I := 1 to 4 do
@@ -720,38 +706,20 @@ begin
     begin
       for K := 1 to Min(R2[I - 1], 5) do
       begin
-        // Begin temp fix for output sprites of Armor smithy
-        if aHouse = ht_ArmorSmithy then
+        if aHouse = ht_ArmorSmithy then // For some reason KaM stores these wares in swapped order, here we fix it
         begin
           if I = 1 then // If sprite 1 gets called, render house sprite 2 (Metalshield)
-          begin
-            Ident2 := 2;
-            Id := gRes.HouseDat[aHouse].SupplyOut[Ident2, K] + 1;
-            AddHouseSupplySprite(Id);
-          end else
-          if I = 2 then // If sprite 2 gets called, render house sprite 1 (MetalArmor)
-          begin
-            Ident2 := 1;
-            Id := gRes.HouseDat[aHouse].SupplyOut[Ident2, K] + 1;
-            AddHouseSupplySprite(Id);
-          end else // Back to normal processing for remaining two entries
-          begin
+            Id := gRes.HouseDat[aHouse].SupplyOut[2, K] + 1
+          else if I = 2 then // If sprite 2 gets called, render house sprite 1 (MetalArmor)
+            Id := gRes.HouseDat[aHouse].SupplyOut[1, K] + 1
+          else // Back to normal processing for remaining two entries
             Id := gRes.HouseDat[aHouse].SupplyOut[I, K] + 1;
-            AddHouseSupplySprite(Id);
-          end
-        end else
-        // End temp fix for output sprites of Armor smithy
-        begin
+        end else // End of fix
           Id := gRes.HouseDat[aHouse].SupplyOut[I, K] + 1;
-          AddHouseSupplySprite(Id);
-        end
-      end
-    end
-    else
-    begin
+      end;
+    end else
       Id := gRes.HouseDat[aHouse].SupplyOut[I, Min(R2[I - 1], 5)] + 1;
-      AddHouseSupplySprite(Id);
-    end;
+    AddHouseSupplySprite(Id);
   end;
 end;
 
