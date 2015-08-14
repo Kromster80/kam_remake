05b89c9bdaa04daae2fc29d88b7ec5babd400691
kromster80 <kromster80@gmail.com>
2015-08-14 07:56:50 +0300

    Clarified comment

--- src/gui/KM_InterfaceGamePlay.pas
+++ src/gui/KM_InterfaceGamePlay.pas
@@ -1857,15 +1857,15 @@ begin
   // Try to highlight the house in question
   H := gHands.HousesHitTest(Msg.Loc.X, Msg.Loc.Y);
 
-  // Check if house exists, then if it does not have owner (but can have one) and it is not Barracks)
-  // highlight it, other condition is when the resources have been depleted for this house
+  // Do not highlight a house if it is not the one that has issued the notification
+  // (happens when note is issues and house is destroyed and a new one is build in the same place)
   // NOTE: It will highlight next house built on the 'ruins' which is unoccupied to be precise
   //       even the NEW message has not been issued yet
   if (H <> nil) then
-    if ((Msg.fTextID = TX_MSG_HOUSE_UNOCCUPIED) and (not H.GetHasOwner) and
-        (gRes.HouseDat[H.HouseType].OwnerType <> ut_None) and (H.HouseType <> ht_Barracks)) or
-        (H.ResourceDepletedMsgIssued) then
-          MySpectator.Highlight := H;
+    if ((Msg.fTextID = TX_MSG_HOUSE_UNOCCUPIED) and not H.GetHasOwner
+        and (gRes.HouseDat[H.HouseType].OwnerType <> ut_None) and (H.HouseType <> ht_Barracks))
+    or H.ResourceDepletedMsgIssued then
+      MySpectator.Highlight := H;
 
   MessageLog_Update(True);
 end;
