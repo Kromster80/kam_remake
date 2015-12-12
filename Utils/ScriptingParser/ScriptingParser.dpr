ef582f68b3e0b43e45f68001d6793c767ec28b1b
thibmo <thibmorozier@live.nl>
2015-12-12 17:25:03 +0100

    Small change

--- Utils/ScriptingParser/ScriptingParser.dpr
+++ Utils/ScriptingParser/ScriptingParser.dpr
@@ -1,14 +1,9 @@
 program ScriptingParser;
 
 uses
-  madExcept,
-  madLinkDisAsm,
-  madListHardware,
-  madListProcesses,
-  madListModules,
   Vcl.Forms,
-  Main in 'src\Main.pas' {Form1},
-  FormHelp in 'src\FormHelp.pas' {helpForm};
+  Main in 'Main.pas' {Form1},
+  FormHelp in 'FormHelp.pas' {helpForm};
 
 {$R *.res}
 
