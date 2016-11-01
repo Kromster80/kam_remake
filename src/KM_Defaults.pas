abcec9e466cf161ff9097fa3cc53c3409e073322
rey <levchenko.and@gmail.com>
2016-11-01 08:36:09 +0300

    Delete field plans by holding left mouse btn
    invoking gic_BuildAddFieldPlan is going from MouseDown and MouseMove
    methods, instead of MouseMove and MouseUp.

--- src/KM_Defaults.pas
+++ src/KM_Defaults.pas
@@ -260,6 +260,13 @@ type
     cmMarkers //CenterScreen, Defence, FOW markers
     );
 
+type
+  TKMCursorFieldMode = (
+    cfmNone,
+    cfmPlan,
+    cfmErase
+  );
+
 const
   MARKER_REVEAL = 1;
   MARKER_DEFENCE = 2;
