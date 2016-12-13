15c08a01feb287a9bf54958beae51230fdfe75ed
Rey <levchenko.and@gmail.com>
2016-12-13 11:48:15 +0300

    max out resources could be > 5

--- src/gui/pages_maped/KM_GUIMapEdHouse.pas
+++ src/gui/pages_maped/KM_GUIMapEdHouse.pas
@@ -340,7 +340,7 @@ begin
 
     if Sender = ResRow_Resource_Output[I].OrderAdd then
     begin
-      NewCount := Math.Min(GetMultiplicator(Shift), MAX_WARES_IN_HOUSE - fHouse.CheckResOut(Res));
+      NewCount := Math.Min(GetMultiplicator(Shift), MAX_WARES_IN_HOUSE + HouseDat.ResProductionX - 1 - fHouse.CheckResOut(Res));
       fHouse.ResAddToOut(Res, NewCount);
     end;
 
