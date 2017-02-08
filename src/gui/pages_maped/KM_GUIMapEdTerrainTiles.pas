7aa41ca270d126bbd319ec7441d3d3aca7d73d5f
Rey <levchenko.and@gmail.com>
2017-02-08 08:31:18 +0300

    mapEd. Added ability to rotate tile. Refactoring

--- src/gui/pages_maped/KM_GUIMapEdTerrainTiles.pas
+++ src/gui/pages_maped/KM_GUIMapEdTerrainTiles.pas
@@ -19,7 +19,7 @@ type
     TilesTable: array [0 .. MAPED_TILES_X * MAPED_TILES_Y - 1] of TKMButtonFlat; //how many are visible?
     TilesScroll: TKMScrollBar;
     TilesRandom: TKMCheckBox;
-    TilesMagicWater, TilesEyedropper: TKMButtonFlat;
+    TilesMagicWater, TilesEyedropper, TilesRotate: TKMButtonFlat;
   public
     constructor Create(aParent: TKMPanel);
 
@@ -70,20 +70,26 @@ begin
   TilesEyedropper.Hint := gResTexts[TX_MAPED_TERRAIN_EYEDROPPER_HINT];
   TilesEyedropper.OnClick := TilesChange;
 
-  TilesRandom := TKMCheckBox.Create(Panel_Tiles, 0, 86, TB_WIDTH, 20, gResTexts[TX_MAPED_TERRAIN_TILES_RANDOM], fnt_Metal);
+  TilesRotate := TKMButtonFlat.Create(Panel_Tiles, 2, 76, TB_WIDTH - 4, 20, 0);
+  TilesRotate.Caption := 'Rotate tile'; //Todo translate;
+  TilesRotate.CapOffsetY := -12;
+  TilesRotate.Hint := 'Rotate tile'; //Todo translate;
+  TilesRotate.OnClick := TilesChange;
+
+  TilesRandom := TKMCheckBox.Create(Panel_Tiles, 0, 112, TB_WIDTH, 20, gResTexts[TX_MAPED_TERRAIN_TILES_RANDOM], fnt_Metal);
   TilesRandom.Checked := True;
   TilesRandom.OnClick := TilesChange;
   TilesRandom.Hint := gResTexts[TX_MAPED_TERRAIN_TILES_RANDOM_HINT];
 
   //Create scroll first to link to its MouseWheel event
-  TilesScroll := TKMScrollBar.Create(Panel_Tiles, 2, 106 + 4 + MAPED_TILES_Y * 32, 194, 20, sa_Horizontal, bsGame);
+  TilesScroll := TKMScrollBar.Create(Panel_Tiles, 2, 142 + 4 + MAPED_TILES_Y * 32, 194, 20, sa_Horizontal, bsGame);
   TilesScroll.MaxValue := 256 div MAPED_TILES_Y - MAPED_TILES_X; // 32 - 6
   TilesScroll.Position := 0;
   TilesScroll.OnChange := TilesRefresh;
   for J := 0 to MAPED_TILES_Y - 1 do
   for K := 0 to MAPED_TILES_X - 1 do
   begin
-    TilesTable[J * MAPED_TILES_X + K] := TKMButtonFlat.Create(Panel_Tiles, K * 32, 106 + J * 32, 32, 32, 1, rxTiles);
+    TilesTable[J * MAPED_TILES_X + K] := TKMButtonFlat.Create(Panel_Tiles, K * 32, 142 + J * 32, 32, 32, 1, rxTiles);
     TilesTable[J * MAPED_TILES_X + K].Tag :=  J * MAPED_TILES_X + K; //Store ID
     TilesTable[J * MAPED_TILES_X + K].OnClick := TilesChange;
     TilesTable[J * MAPED_TILES_X + K].OnMouseWheel := TilesScroll.MouseWheel;
@@ -93,14 +99,27 @@ end;
 
 procedure TKMMapEdTerrainTiles.TilesChange(Sender: TObject);
 begin
-  TilesMagicWater.Down := (Sender = TilesMagicWater);
-  TilesEyedropper.Down := (Sender = TilesEyedropper);
+  TilesMagicWater.Down := (Sender = TilesMagicWater) and not TilesMagicWater.Down;
+  TilesEyedropper.Down := (Sender = TilesEyedropper) and not TilesEyedropper.Down;
+  TilesRotate.Down := (Sender = TilesRotate) and not TilesRotate.Down;
 
   if Sender = TilesMagicWater then
-    gGameCursor.Mode := cmMagicWater;
+    if TilesMagicWater.Down then
+      gGameCursor.Mode := cmMagicWater
+    else
+      gGameCursor.Mode := cmNone;
 
   if Sender = TilesEyedropper then
-    gGameCursor.Mode := cmEyedropper;
+    if TilesEyedropper.Down then
+      gGameCursor.Mode := cmEyedropper
+    else
+      gGameCursor.Mode := cmNone;
+
+  if Sender = TilesRotate then
+    if TilesRotate.Down then
+      gGameCursor.Mode := cmRotateTile
+    else
+      gGameCursor.Mode := cmNone;
 
   if Sender = TilesRandom then
     gGameCursor.MapEdDir := 4 * Byte(TilesRandom.Checked); //Defined=0..3 or Random=4
@@ -165,6 +184,7 @@ begin
     //If cursor has a tile then make sure its properly selected in table as well
     TilesTable[L].Down := (gGameCursor.Mode = cmTiles) and (gGameCursor.Tag1 = TileID - 1);
     TilesEyedropper.Down := gGameCursor.Mode = cmEyedropper;
+    TilesRotate.Down := gGameCursor.Mode = cmRotateTile;
   end;
 end;
 
