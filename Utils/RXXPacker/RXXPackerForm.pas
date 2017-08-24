3706279b38958b1e2a0a5d8b7549f7ce527d2552
Rey <levchenko.and@gmail.com>
2017-08-24 18:20:47 +0300

    make soft shadows for woodcutter cutting point sign

--- Utils/RXXPacker/RXXPackerForm.pas
+++ Utils/RXXPacker/RXXPackerForm.pas
@@ -174,6 +174,7 @@ begin
             SpritePack.SoftenShadows(105, 128); //Field plans
             SpritePack.SoftenShadows(249, 281); //House tablets only (shadow softening messes up other rxGui sprites)
             SpritePack.SoftenShadows(461, 468); //Field fences
+            SpritePack.SoftenShadows(660, 660); //Woodcutter cutting point sign
           end
           else
             SpritePack.SoftenShadows;
