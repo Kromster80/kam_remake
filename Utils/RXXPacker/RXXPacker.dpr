ef8c2c382c2f884407702dd48856c166c32f0bf7
Rey <levchenko.and@gmail.com>
2017-09-08 18:26:09 +0300

    Added console mode for RXXPacker

--- Utils/RXXPacker/RXXPacker.dpr
+++ Utils/RXXPacker/RXXPacker.dpr
@@ -1,21 +1,67 @@
 program RXXPacker;
 {$I ..\..\KaM_Remake.inc}
 uses
-  Forms,
+  Forms, SysUtils,
   {$IFDEF FPC}Interfaces,{$ENDIF}
+  {$IFDEF MSWindows} Windows, {$ENDIF}
+  {$IFDEF FPC} LResources, LCLIntf, {$ENDIF}
   RXXPackerForm in 'RXXPackerForm.pas' {RXXForm1},
+	RXXPackerProc in 'RXXPackerProc.pas',
   KM_PNG in '..\..\src\utils\KM_PNG.pas',
+  KM_Pics in '..\..\src\utils\KM_Pics.pas',
   KM_ResSprites in '..\..\src\res\KM_ResSprites.pas',
   KM_ResSpritesEdit in '..\..\src\res\KM_ResSpritesEdit.pas',
-  KM_SoftShadows in '..\..\src\KM_SoftShadows.pas';
+  KM_ResPalettes in '..\..\src\res\KM_ResPalettes.pas',
+  KM_SoftShadows in '..\..\src\KM_SoftShadows.pas',
+  KM_Defaults in '..\..\src\common\KM_Defaults.pas';
 
+	{$APPTYPE CONSOLE}
 {$IFDEF WDC}
 {$R *.res}
 {$ENDIF}
 
+var
+  I: Integer;
+  J: TRXType;
+  fRXXPacker: TRXXPacker;
+  fPalettes: TKMResPalettes;
+  Tick: Cardinal;
 
 begin
-  Application.Initialize;
-  Application.CreateForm(TRXXForm1, RXXForm1);
-  Application.Run;
+  if ParamCount >= 1 then
+  begin
+    writeln(sLineBreak + 'KaM Remake RXX Packer' + sLineBreak);
+
+    if ParamCount = 0 then
+    begin
+      writeln('No rx packages were set');
+      writeln('Usage example: RXXPacker.exe gui guimain houses trees units');
+      Exit;
+    end;
+
+
+    ExeDir := ExpandFileName(ExtractFilePath(ParamStr(0)) + '..\..\');
+    fRXXPacker := TRXXPacker.Create;
+    fPalettes := TKMResPalettes.Create;
+    fPalettes.LoadPalettes(ExeDir + 'data\gfx\');
+    try
+      for I := 1 to ParamCount do // Skip 0, as this is the EXE-path
+        for J := Low(TRXType) to High(TRXType) do
+          if (LowerCase(ParamStr(I)) = LowerCase(RXInfo[J].FileName)) then
+          begin
+            Tick := GetTickCount;
+            fRXXPacker.Pack(J, fPalettes);
+            writeln(RXInfo[J].FileName + '.rxx packed in ' + IntToStr(GetTickCount - Tick) + ' ms');
+          end;
+    finally
+      fRXXPacker.Free;
+      fPalettes.Free;
+    end;
+
+
+  end else begin
+    Application.Initialize;
+    Application.CreateForm(TRXXForm1, RXXForm1);
+    Application.Run;
+  end;
 end.
--- Utils/RXXPacker/RXXPackerForm.pas
+++ Utils/RXXPacker/RXXPackerForm.pas
@@ -6,7 +6,7 @@ uses
   ExtCtrls, Forms, Graphics, Spin, StdCtrls, SysUtils, TypInfo,
   {$IFDEF MSWindows} Windows, {$ENDIF}
   {$IFDEF FPC} LResources, LCLIntf, {$ENDIF}
-  KM_Defaults, KM_Log, KM_Pics, KM_ResPalettes, KM_ResSprites, KM_ResSpritesEdit;
+  RXXPackerProc, KM_Defaults, KM_Log, KM_Pics, KM_ResPalettes;
 
 
 type
@@ -19,6 +19,7 @@ type
     procedure FormDestroy(Sender: TObject);
   private
     fPalettes: TKMResPalettes;
+    fRxxPacker: TRXXPacker;
   end;
 
 
@@ -42,6 +43,7 @@ begin
   //Although we don't need them in this tool, these are required to load sprites
   gLog := TKMLog.Create(ExeDir + 'RXXPacker.log');
 
+  fRXXPacker := TRXXPacker.Create;
   fPalettes := TKMResPalettes.Create;
   fPalettes.LoadPalettes(ExeDir + 'data\gfx\');
 
@@ -56,36 +58,15 @@ procedure TRXXForm1.FormDestroy(Sender: TObject);
 begin
   FreeAndNil(fPalettes);
   FreeAndNil(gLog);
+  FreeAndNil(fRXXPacker);
 end;
 
 
 procedure TRXXForm1.btnPackRXXClick(Sender: TObject);
 var
-  DeathAnimProcessed: array of Integer;
-  DeathAnimCount: Integer;
-
-  function DeathAnimAlreadyDone(aID:Integer):Boolean;
-  var I:Integer;
-  begin
-    Result := False;
-    for I:=0 to DeathAnimCount-1 do
-      if DeathAnimProcessed[I] = aID then
-      begin
-        Result := True;
-        Exit;
-      end;
-  end;
-
-var
-  SpritePack: TKMSpritePackEdit;
   RT: TRXType;
-  I, Step, SpriteID: Integer;
+  I: Integer;
   Tick: Cardinal;
-  RXName: string;
-  resHouses: TKMResHouses;
-  resUnits: TKMResUnits;
-  UT: TUnitType;
-  Dir: TKMDirection;
 begin
   btnPackRXX.Enabled := False;
   Tick := GetTickCount;
@@ -98,93 +79,8 @@ begin
   if ListBox1.Selected[I] then
   begin
     RT := TRXType(I);
-    //ruCustom sprite packs do not have a main RXX file so don't need packing
-    if RXInfo[RT].Usage <> ruCustom then
-    begin
-      RXName := ExeDir + 'SpriteResource\' + RXInfo[RT].FileName + '.rx';
-      Assert((RT = rxTiles) or FileExists(RXName),
-             'Cannot find ' + RXName + ' file.'+#10#13+
-             'Please copy the file from your KaM\data\gfx\res\ folder.');
 
-      SpritePack := TKMSpritePackEdit.Create(RT, fPalettes);
-      try
-        //Load
-        if RT <> rxTiles then
-        begin
-          SpritePack.LoadFromRXFile(RXName);
-          SpritePack.OverloadFromFolder(ExeDir + 'SpriteResource\');
-        end
-        else
-        if DirectoryExists(ExeDir + 'SpriteResource\') then
-          SpritePack.LoadFromFolder(ExeDir + 'SpriteResource\');
-
-        //Tiles must stay the same size as they can't use pivots
-        if RT <> rxTiles then
-          gLog.AddTime('Trimmed ' + IntToStr(SpritePack.TrimSprites));
-
-        //Houses need some special treatment to adapt to GL_ALPHA_TEST that we use for construction steps
-        if RT = rxHouses then
-        begin
-          resHouses := TKMResHouses.Create;
-          SpritePack.AdjoinHouseMasks(resHouses);
-          SpritePack.GrowHouseMasks(resHouses);
-          resHouses.Free;
-        end;
-
-        //The idea was to blur the water and make it semitrasparent, but it did not worked out as expected
-        //if RT = rxTiles then
-        //  SpritePack.SoftWater(nil);
-
-        //Save
-        SpritePack.SaveToRXXFile(ExeDir + 'Data\Sprites\' + RXInfo[RT].FileName + '.rxx');
-
-        //Generate alpha shadows for the following sprite packs
-        if RT in [rxHouses,rxUnits,rxGui,rxTrees] then
-        begin
-          if RT = rxHouses then
-          begin
-            SpritePack.SoftenShadows(889, 892, False); //Smooth smoke
-            SpritePack.SoftenShadows(1615, 1638, False); //Smooth flame
-          end;
-          if RT = rxUnits then
-          begin
-            SpritePack.SoftenShadows(6251, 6314, False); //Smooth thought bubbles
-            //Smooth all death animations for all units
-            resUnits := TKMResUnits.Create;
-            DeathAnimCount := 0; //We need to remember which ones we've done because units reuse them
-            SetLength(DeathAnimProcessed, 1000); //Hopefully more than enough
-            for UT := HUMANS_MIN to HUMANS_MAX do
-              for Dir := dir_N to dir_NW do
-                for Step := 1 to 30 do
-                begin
-                  SpriteID := resUnits[UT].UnitAnim[ua_Die,Dir].Step[Step]+1; //Sprites in units.dat are 0 indexed
-                  if (SpriteID > 0)
-                  and not DeathAnimAlreadyDone(SpriteID) then
-                  begin
-                    SpritePack.SoftenShadows(SpriteID, False);
-                    DeathAnimProcessed[DeathAnimCount] := SpriteID;
-                    inc(DeathAnimCount);
-                  end;
-                end;
-            resUnits.Free;
-          end;
-
-          if RT = rxGui then
-          begin
-            SpritePack.SoftenShadows(105, 128); //Field plans
-            SpritePack.SoftenShadows(249, 281); //House tablets only (shadow softening messes up other rxGui sprites)
-            SpritePack.SoftenShadows(461, 468); //Field fences
-            SpritePack.SoftenShadows(660, 660); //Woodcutter cutting point sign
-          end
-          else
-            SpritePack.SoftenShadows;
-
-          SpritePack.SaveToRXXFile(ExeDir + 'Data\Sprites\' + RXInfo[RT].FileName + '_a.rxx');
-        end;
-      finally
-        SpritePack.Free;
-      end;
-    end;
+    fRxxPacker.Pack(RT, fPalettes);
 
     ListBox1.Selected[I] := False;
     ListBox1.Refresh;
