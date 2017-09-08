16b608f995b6279094bb18e578b187d39e2c67c7
Rey <levchenko.and@gmail.com>
2017-09-08 14:29:23 +0300

    Fix application icon in delphi

--- KaM_Remake.dpr
+++ KaM_Remake.dpr
@@ -278,8 +278,8 @@ uses
   KM_WareDistribution in 'src\KM_WareDistribution.pas';
 
 {$IFDEF WDC}
-  {$R KaM_Remake_Icon.res KaM_Remake_Icon.rc} //Keeps the Icon
-  {$R KaM_Manifest.res KaM_Manifest.rc}
+  {$R KaM_Remake_Icon.res} //Keeps the Icon
+  {$R KaM_Manifest.res}
 {$ENDIF}
 {$IFDEF MSWindows}
   {$IFDEF FPC}
