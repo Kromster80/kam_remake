70b342d9be801009185216d08ba6a574b193df82
Rey <levchenko.and@gmail.com>
2017-09-13 18:06:02 +0300

    bat_revision number

--- bat/bat_get_kam_folder.bat
+++ bat/bat_get_kam_folder.bat
@@ -23,8 +23,8 @@ REM for /F "delims=" %%i in (KM_Revision.inc) do set "verinfo=%%i"
 
 REM Replace ' with nothing to get the version info
 REM @SET kam_revision=%verinfo:'=%
-@SET kam_revision=1
+@SET kam_revision=2
 
 REM Now we can have a constant with the right folder name
-@SET kam_folder=%BetaVersionsDir%\kam%YYYY%-%MM%-%DD% (%kam_version% br%kam_revision%)
+@SET kam_folder=%BetaVersionsDir%\kam%YYYY%-%MM%-%DD% (%kam_version% rev%kam_revision%)
 echo kam_folder: "%kam_folder%"
